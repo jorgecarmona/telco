0664fd0d26d80b80468fd947b9def98435175e25
Andrea Carmona <andrea@carmonaweb.com>
2024-07-21 12:10:52 -0400

    feat(tabs): added orientation, icons, scroll, disabled

--- src/atoms/tab-panel.tsx
+++ src/atoms/tab-panel.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
+
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 
+import {IconType, iconLookup} from '../atoms/icon-store';
+
 interface TabConfig {
   label: string;
   value: number;
   component: React.ReactNode;
+  icon?: IconType;
+  disabled?: boolean;
   callBack?: () => void;
 }
 
 interface TabPanelProps {
   tabs: TabConfig[];
-  //orientacion: vertical y horizontal
+  orientation?: 'horizontal' | 'vertical';
 }
 
-export default function TabPanel({ tabs }: TabPanelProps) {
-  const [value, setValue] = React.useState<number>(tabs[0]?.value || 0);
+function TabPanel({ tabs, orientation = 'horizontal' }: TabPanelProps) {
+  const [value, setValue] = React.useState<number>(tabs[1]?.value || 0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
-
+    
     if (tabs[newValue] && typeof tabs[newValue].callBack === 'function') {
       tabs[newValue].callBack?.();
     }
@@ -29,14 +34,30 @@ export default function TabPanel({ tabs }: TabPanelProps) {
 
   return (
     <Box>
-      <Tabs onChange={handleChange} value={value}>
-        {tabs.map((tab) => (
-          <Tab key={tab.value} label={tab.label} value={tab.value} />
-        ))}
+      <Tabs
+        onChange={handleChange}
+        value={value}
+        variant={tabs.length > 5 ? 'scrollable' : 'standard'} 
+        scrollButtons="auto"
+        orientation={orientation}
+      >
+        {tabs.map((tab) => {
+          const Icon = tab.icon ? iconLookup[tab.icon] : undefined;
+          
+          return <Tab
+            key={tab.value}
+            label={tab.label}
+            icon={Icon && <Icon/>}
+            disabled={tab.disabled}
+            value={tab.value}
+          />
+        })}
       </Tabs>
       <React.Suspense fallback={<CircularProgress />}>
-        {tabs.find(tab => tab.value === value)?.component}
+        {tabs.find((tab) => tab.value === value)?.component}
       </React.Suspense>
     </Box>
   );
 }
+
+export default TabPanel;
