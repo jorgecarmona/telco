a89de45396377ec0561316696b1dac8b30722441
Oskar Mantilla <oskarmantilla1708@gmail.com>
2024-07-26 10:09:53 -0500

    feat(pagination-data-grid): added pagination props

--- src/atoms/data-grid.tsx
+++ src/atoms/data-grid.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { DataGrid as MuiDataGrid, DataGridProps as MuiDataGridProps, GridColDef } from '@mui/x-data-grid';
+import React, {useState} from 'react';
+import { DataGrid as MuiDataGrid, DataGridProps as MuiDataGridProps, GridColDef, GridPaginationModel} from '@mui/x-data-grid';
 
 interface DataGridProps extends Omit<MuiDataGridProps, 'columns' | 'rows'> {
   columns: GridColDef[];
   rows: Record<string, unknown>[];
+  pageSize?: number;
+  pageSizeOptions?: number[];
 }
 
-function DataGrid({ columns, rows, ...other }: DataGridProps) {
+function DataGrid({ columns, rows, pageSize = 0, pageSizeOptions = [], ...other}: DataGridProps) {
+  const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
+    pageSize: pageSize,
+    page: 0,
+  });
+  const handlePaginationModelChange = (model: GridPaginationModel) => {
+    setPaginationModel(model);
+  };
   return (
-      <div style={{ height: 700}}>
-        <MuiDataGrid 
+    <div style={{ height: 600, width: '100%' }}>
+      <MuiDataGrid 
         rows={rows} 
         columns={columns} 
-        {...other} />
-      </div>
+        paginationModel={paginationModel}
+        onPaginationModelChange={handlePaginationModelChange}
+        pageSizeOptions={pageSizeOptions}
+        {...other}
+      />
+    </div>
   );
 }
 
