25f136922cc359b9da6298f90a37c376eddd25b5
David Cedillo <137354680+davidc0326@users.noreply.github.com>
2024-08-02 16:55:57 -0400

    feat(datagrid): added sorting stories functionality and test

--- src/stories/atoms/data-grid.stories.tsx
+++ src/stories/atoms/data-grid.stories.tsx
@@ -1,22 +1,24 @@
-import { Meta,  StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { GridColDef } from '@mui/x-data-grid';
 
 import DataGrid from '../../atoms/data-grid';
 
 const meta: Meta<typeof DataGrid> = {
-    title: 'Atoms/DataGrid',
-    component: DataGrid,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
-    argTypes: {
-        disableColumnFilter: { control: 'boolean' },
-        columns: { table: { disable: true } },
-        rows: { table: { disable: true } },
-        filterModel: { table: { disable: true } },
-    },
+  title: 'Atoms/DataGrid',
+  component: DataGrid,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    columns: { table: { disable: true } },
+    disableColumnFilter: { control: 'boolean' },
+    disableColumnSorting: { control: 'boolean' },
+    filterModel: { table: { disable: true } },
+    rows: { table: { disable: true } },
+    sortModel: { control: 'object' },
+  },
 } satisfies Meta<typeof DataGrid>;
 
 export default meta;
@@ -24,41 +26,52 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
+  args: {
     columns: [
-        { field: 'id', headerName: 'ID', width: 90 },
-        { field: 'firstName', headerName: 'First name', width: 150 },
-        { field: 'lastName', headerName: 'Last name', width: 150 },
-        { field: 'age', headerName: 'Age', type: 'number', width: 110 },
+      { field: 'id', headerName: 'ID', width: 90 },
+      { field: 'firstName', headerName: 'First name', width: 150 },
+      { field: 'lastName', headerName: 'Last name', width: 150 },
+      { field: 'age', headerName: 'Age', type: 'number', width: 110 },
     ] as GridColDef[],
-    rows: [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    ],
     disableColumnFilter: false,
+    disableColumnSorting: false,
     filterModel: { items: [] },
-    },
+    rows: [
+      { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+      { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+      { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    ],
+    sortModel: [{ field: 'firstName', sort: 'asc' }],
+  },
 };
 
 export const WithoutFilter: Story = {
-    args: {
-        ...Default.args,
-        disableColumnFilter: true,
-    },
+  args: {
+    ...Default.args,
+    disableColumnFilter: true,
+  },
+};
+
+export const WithoutSorting: Story = {
+  args: {
+    ...Default.args,
+    disableColumnSorting: true,
+  },
 };
 
 export const Empty: Story = {
-    args: {
+  args: {
     columns: [
-        { field: 'id', headerName: 'ID', width: 90 },
-        { field: 'firstName', headerName: 'First name', width: 150 },
-        { field: 'lastName', headerName: 'Last name', width: 150 },
-        { field: 'age', headerName: 'Age', type: 'number', width: 110 },
+      { field: 'id', headerName: 'ID', width: 90 },
+      { field: 'firstName', headerName: 'First name', width: 150 },
+      { field: 'lastName', headerName: 'Last name', width: 150 },
+      { field: 'age', headerName: 'Age', type: 'number', width: 110 },
     ] as GridColDef[],
-    rows: [],
     disableColumnFilter: false,
+    disableColumnSorting: false,
     filterModel: { items: [] },
-    }
+    rows: [],
+    sortModel: [],
+  },
 };
