9a7efefaaf123dd79eee9faa1db5206036a626b3
Juana Valentina <jvalentina0709@gmail.com>
2024-08-05 20:03:47 -0500

    refactor(alert):change were made to the comment

--- src/stories/atoms/alert.stories.tsx
+++ src/stories/atoms/alert.stories.tsx
@@ -1,7 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
 
 import Alert from '../../atoms/alert';
-import {action} from '@storybook/addon-actions';
 
 const meta = {
     title: 'Atoms/Alert',
